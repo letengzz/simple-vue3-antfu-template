3379a1ce0f5d26d6110d201546cf956415d097f7
letengzz <2020885569@qq.com>
2025-03-19 18:43:18 +0800

    chore: update unocss

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { useRouter } from './router'
 import { usePinia } from './stores'
 
 import 'virtual:uno.css' // 引入 uno.css
-
+import '@unocss/reset/sanitize/sanitize.css'
+import '@unocss/reset/sanitize/assets.css'
 // 引入全局样式
 import '@/styles/index.scss'
 
